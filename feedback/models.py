d1febd3303ea6176b5234f95bf70b8775427c3c0
gu-ss-tavo <gusstavo.jarabs@gmail.com>
2022-01-22 19:13:52 -0600

    first commit - configured

---
+++ feedback/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+from post.models import Post
+
+# Create your models here.
+class FeedBack(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'{self.user} -> {self.body}'
---
+++ readme.md
@@ -0,0 +1,14 @@
+
+## **Academlo News** :notebook:
+
+---
+
+**Start in development mode**
+
+>`python3 manage.py runserver`
+
+---
+
+by **gu-ss-tavo** :skull:
+
+---
